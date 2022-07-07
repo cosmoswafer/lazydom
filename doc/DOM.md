8ad3cf66ef21b2ce3b0831626fc6771c5bfd055f
Jimmy.|\| <jimmy09140456@gmail.com>
2022-07-07 09:19:45 +0800

    Prettier format (#12)

--- doc/DOM.md
+++ doc/DOM.md
@@ -1,14 +1,16 @@
 # DOM usage
 
 Two cases:
+
 1. DOM methods shortcut, use normal selector to look up elements
 2. Lazy template, look up the template element and update its contents
 
 The shortcuts:
+
 1. Query element and cache them to improve the performance
 2. The `dot` method to quickly update set of contents
 3. Binding event listeners as well by the above methods
 
-*Remarks:*
-1. The lazy template will clone itself and clean up automatically.
+_Remarks:_
 
+1. The lazy template will clone itself and clean up automatically.
--- router.js
+++ router.js
@@ -1,56 +1,55 @@
 export class Router {
-    static #router = null;
+  static #router = null;
 
-    constructor(default_page = '') {
-        if (Router.#router != null) return Router.#router;
+  constructor(default_page = "") {
+    if (Router.#router != null) return Router.#router;
 
-        Router.#router = this;
-        this.current_page = default_page;
-        this.pages = [];
-        window.addEventListener('hashchange', this.#pageChange);
+    Router.#router = this;
+    this.current_page = default_page;
+    this.pages = [];
+    window.addEventListener("hashchange", this.#pageChange);
+  }
+
+  addPages(...ids) {
+    ids.forEach((p) => this.pages.push(p));
+  }
+
+  #pageChange = (e) => {
+    const page_id = location.hash.replace("#", "");
+    this.render(page_id);
+  };
+
+  render(page_id) {
+    if (!this.pages.includes(page_id)) return;
+
+    for (let p of this.pages) {
+      const p_div = document.querySelector(`#${p}`);
+
+      if (p_div) p_div.style.display = p === page_id ? "" : "none";
     }
+  }
 
-    addPages(...ids) {
-        ids.forEach((p) => this.pages.push(p));
-    }
-
-    #pageChange = (e) => {
-        const page_id = location.hash.replace('#', '');
-        this.render(page_id);
-    };
-
-    render(page_id) {
-        if (!this.pages.includes(page_id)) return;
-
-        for (let p of this.pages) {
-            const p_div = document.querySelector(`#${p}`);
-
-            if (p_div) p_div.style.display = p === page_id ? '' : 'none';
-        }
-    }
-
-    goTo(page_id) {
-        location.hash === `#${page_id}`
-            ? this.render(page_id)
-            : (location.hash = page_id);
-    }
+  goTo(page_id) {
+    location.hash === `#${page_id}`
+      ? this.render(page_id)
+      : (location.hash = page_id);
+  }
 }
 
 export class PageBase {
-    router = new Router();
-    #pid;
-    page_name;
+  router = new Router();
+  #pid;
+  page_name;
 
-    constructor(page_name) {
-        this.#pid = page_name;
-        this.page_name = page_name;
-        this.router.addPages(this.#pid);
-        //Convert page name into CSS id format
-        this.dom = new DOM(`#${this.#pid}`);
-    }
+  constructor(page_name) {
+    this.#pid = page_name;
+    this.page_name = page_name;
+    this.router.addPages(this.#pid);
+    //Convert page name into CSS id format
+    this.dom = new DOM(`#${this.#pid}`);
+  }
 
-    show() {
-        this.router.goTo(this.#pid);
-    }
+  show() {
+    this.router.goTo(this.#pid);
+  }
 }
-
